e0889ff7c67a33d62a9d80d651368da2226b8014
Brad Lindsay <br.mi.lind@gmail.com>
2025-02-09 14:53:03 +0100

    Initial Checkin

---
+++ Ola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ola;
+
+internal static class Program
+{
+    public static Task Main(string[] args)
+    {
+        Console.WriteLine("OLA - Open License Attribution");
+        return Task.CompletedTask;
+    }
+}
